157dfd84cbf24b3085162cf18a99eaee7717c1e2
Uttam Prajapati <uttam.prajapati@rysun.com>
2026-05-12 15:27:09 +0530

    Project intial

---
+++ Backend/FlowDesk.Api/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FlowDesk.Application;
+using FlowDesk.Domain;
+using FlowDesk.Infrastructure;
+
+
+namespace FlowDesk.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services
+            .AddApplication()
+            .AddDomain()
+            .AddInfrastructure(configuration);
+
+            return services;
+        }
+    }
+}
---
+++ Backend/FlowDesk.Api/Program.cs
@@ -0,0 +1,55 @@
+using FlowDesk.Api;
+using FlowDesk.Api.Middleware;
+using FlowDesk.Application.Common.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//Inject AddApi Dependencies
+builder.Services.AddApi(builder.Configuration);
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(x => x.Value?.Errors.Count > 0)
+            .SelectMany(x => x.Value?.Errors!)
+            .Select(x => x.ErrorMessage)
+            .ToList();
+
+        var response = new ErrorResponseDto
+        {
+            StatusCode = 400,
+            Message = "Validation failed",
+            Errors = errors,
+            TraceId = context.HttpContext.TraceIdentifier
+        };
+
+        return new BadRequestObjectResult(response);
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
