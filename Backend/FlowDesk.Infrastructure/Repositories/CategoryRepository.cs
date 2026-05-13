9d0cb763154cd0cb91e35c3cced4805fdbbd4b23
Uttam Prajapati <uttam.prajapati@rysun.com>
2026-05-13 14:32:56 +0530

    feat: implement all repos for entities

---
+++ Backend/FlowDesk.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using FlowDesk.Infrastructure.Data;
+using FlowDesk.Domain.Interfaces;
+using FlowDesk.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowDesk.Infrastructure.Repositories
+{
+    public class CategoryRepository(AppDbContext _context) : ICategoryRepository
+    {
+        public async Task AddAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(int categoryId)
+        {
+            return await _context.Categories.AnyAsync(c => c.CategoryId == categoryId);
+        }
+
+        public async Task<IReadOnlyList<Category>> GetAllAsync()
+        {
+            return await _context.Categories.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Category?> GetByIdAsync(int categoryId)
+        {
+            return _context.Categories.FirstOrDefaultAsync(c => c.CategoryId == categoryId);
+        }
+
+        public async Task HardDelete(Category category)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ToggleActive(Category category)
+        {
+            category.IsActive = !category.IsActive;
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
---
+++ Backend/FlowDesk.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,28 @@
+﻿using FlowDesk.Domain.Entities;
+using FlowDesk.Domain.Interfaces;
+using FlowDesk.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowDesk.Infrastructure.Repositories
+{
+    public class RoleRepository(AppDbContext _context) : IRoleRepository
+    {
+        public async Task<IReadOnlyList<Role>> GetAllAsync()
+        {
+            return await _context.Roles.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Role?> GetByIdAsync(int roleId)
+        {
+            return _context.Roles.AsNoTracking().FirstOrDefaultAsync(r => r.RoleId == roleId);
+        }
+
+        public Task<Role?> GetByName(string roleName)
+        {
+            return _context.Roles.AsNoTracking().FirstOrDefaultAsync(r => r.RoleName == roleName);
+        }
+    }
+}
