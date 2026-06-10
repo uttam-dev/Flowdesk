f068c66178898528b3f3f63091a173906cb039e0
Uttam Prajapati <uttam.prajapati@rysun.com>
2026-06-10 18:13:31 +0530

    feat: added mapping for remote session DTOs

--- Backend/FlowDesk.Application/Common/Mappings/MappingProfile.cs
+++ Backend/FlowDesk.Application/Common/Mappings/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using FlowDesk.Application.Features.Categories.DTOs;
+using FlowDesk.Application.Features.Remote.DTOs;
 using FlowDesk.Application.Features.Requests.DTOs;
 using FlowDesk.Application.Features.Users.DTOs;
 using FlowDesk.Domain.Entities;
@@ -53,9 +54,39 @@ namespace FlowDesk.Application.Common.Mappings
             CreateMap<EscalationHistory, EscalationHistoryDto>()
                         .ForMember(d => d.EscalatedByName,
                             opt => opt.MapFrom(s => s.EscalatedByUser != null ? s.EscalatedByUser.FullName : string.Empty));
-           //Comment
-           CreateMap<Comment, CommentResponseDto>();
+            //Comment
+            CreateMap<Comment, CommentResponseDto>();
 
+            //Remote session
+            CreateMap<RemoteSession, RemoteSessionDto>()
+                    .ForMember(dest => dest.Id,
+                        opt => opt.MapFrom(src => src.RemoteSessionId))
+
+                    .ForMember(dest => dest.InitiatedByName,
+                        opt => opt.MapFrom(src => src.InitiatedBy != null
+                            ? src.InitiatedBy.FullName
+                            : string.Empty))
+
+                    .ForMember(dest => dest.TargetUserName,
+                        opt => opt.MapFrom(src => src.TargetUser != null
+                            ? src.TargetUser.FullName
+                            : string.Empty))
+
+                    .ForMember(dest => dest.Status,
+                        opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<RemoteSession, RemoteSessionDto>()
+    .ForMember(d => d.Id,
+        o => o.MapFrom(s => s.RemoteSessionId))
+
+    .ForMember(d => d.InitiatedByName,
+        o => o.MapFrom(s => s.Request.AssignedUser.FullName))
+
+    .ForMember(d => d.TargetUserName,
+        o => o.MapFrom(s => s.Request.Employee.FullName))
+
+    .ForMember(d => d.Status,
+        o => o.MapFrom(s => s.Status.ToString()));
         }
     }
 }
